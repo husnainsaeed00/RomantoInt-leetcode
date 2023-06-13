06aedb6a7145977488f15d56489fcc9f73c97a7d
Husnain Saeed <husnainsaeed000@gmail.com>
2023-06-13 04:26:08 -0700

    Add files via upload

---
+++ romantoint.py
@@ -0,0 +1,26 @@
+class Solution:
+    def romanToInt(self, s):
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        for c in s:
+            value = roman_values[c]
+
+            if value > prev_value:
+                total += value - 2 * prev_value
+            else:
+                total += value
+
+            prev_value = value
+
+        return total
